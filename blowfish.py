75464d1fee82dcaeb34fafaf503c56de65ce775d
johnk1729 <36747091+johnk1729@users.noreply.github.com>
2020-11-18 12:38:27 -0500

    blowfish.py subkey bug fix

--- blowfish.py
+++ blowfish.py
@@ -11,10 +11,12 @@ import math
 
 class Blowfish:
     __numericKey = 0
+    __subKeys = []
+    __sBoxes = []
 
-    def keyToAsciiCode(self, stringKey):
+    def stringToAsciiInt(self, string):
         numericKey = ''
-        for x in stringKey:
+        for x in string:
             numericKey = int(str(ord(x)) + str(numericKey))
         return numericKey
 
@@ -27,15 +29,16 @@ class Blowfish:
     def createSubKeys(self):
         subKeys = []
         for x in range(18):
-            print(self.__numericKey)
             subKeys.append(hex(abs(int(math.pi*(self.__numericKey ** 1/(x+1)))))[2:10])
+        return subKeys
 
     def __init__(self, key):
-        self.__numericKey = self.keyToAsciiCode(key)
+        self.__numericKey = self.stringToAsciiInt(key)
+        self.__subKeys = self.createSubKeys()
 
 # For testing purposes:
 def main():
-    test = Blowfish('password1')
+    test = Blowfish('password2')
     print(test.createSubKeys())
 
 if __name__ == '__main__':
