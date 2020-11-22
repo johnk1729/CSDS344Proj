479a42cae48071f0191fa508406de91b092451a0
johnk1729 <johnkwak7@gmail.com>
2020-11-22 14:50:09 -0500

    Blowfish added hex and decimal helper functions

--- blowfish.py
+++ blowfish.py
@@ -14,18 +14,29 @@ class Blowfish:
     __subKeys = []
     __sBoxes = []
 
+    #Takes in a string and returns a numeric value based on the ASCII codes of the string
     def stringToAsciiInt(self, string):
         numericKey = ''
         for x in string:
             numericKey = int(str(ord(x)) + str(numericKey))
         return numericKey
+    
+    def decimalToHex(self, decimalValue):
+        return hex(int(decimalValue))[2:]
 
+    #Takes in a hexadecimal value as string and returns a binary value as string
     def hexToBinary(self, hexValue):
+        return bin(self.hexToDecimal(hexValue))[2:]
+    
+    #Takes in a hexadecimal value as string and returns a decimal value as int
+    def hexToDecimal(self, hexValue):
         return int(hexValue, 16)
-
+    
+    #Takes in a binary value as a string or int and returns hexadecimal value as string
     def binaryToHex(self, binaryValue):
         return hex(int(str(binaryValue), 2))[2:]
 
+    #Creates and returns 18 subkeys of 32 bits in hexadecimal based on the numeric key
     def createSubKeys(self):
         subKeys = []
         for x in range(18):
@@ -35,6 +46,7 @@ class Blowfish:
             subKeys.append(subKeyValue)
         return subKeys
     
+    #Creates and returns 4 substitution boxes of 32 rows of 8 columns of 32 bits in hexadecimal based on the numeric key
     def createSBoxes(self):
         sBoxes = []
         for x in range(4):
@@ -50,9 +62,11 @@ class Blowfish:
             sBoxes.append(sbox)
         return sBoxes
     
-    def hexAdd()
-        
+    #Takes in two strings of hexadecimal values and returns a string of the sum
+    def hexAdd(self, hexNum1, hexNum2):
+        return self.decimalToHex(self.hexToDecimal(hexNum1)+self.hexToDecimal(hexNum2))
 
+    #Constructor that takes a string key
     def __init__(self, key):
         self.__numericKey = self.stringToAsciiInt(key)
         random.seed(abs(int(math.pi*(self.__numericKey))))
@@ -62,7 +76,7 @@ class Blowfish:
 # For testing purposes:
 def main():
     test = Blowfish('password')
-    print(test.createSubKeys())
+    print(test.hexAdd('7','8'))
 
 if __name__ == '__main__':
     main()
